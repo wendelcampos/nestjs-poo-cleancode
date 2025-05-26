535b9d1c036cfee0bbcdee022b2295a0a9ae4077
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-05-26 08:05:29 -0300

    feat: implemented prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { Prisma } from 'generated/prisma';
 import { PrismaService } from './prisma/prisma.service';
-
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [PrismaService],
 })
 export class AppModule {}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,17 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
 
 @Injectable()
-export class PrismaService {
-
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    constructor() {
+        super({
+            log: ["warn", "error"],
+        })
+    }
+    onModuleInit() {
+        return this.$connect()
+    }
+    onModuleDestroy() {
+        return this.$disconnect()
+    }
 }
