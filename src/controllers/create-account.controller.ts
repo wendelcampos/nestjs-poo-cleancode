a5e74717dfa9e5b814e92d28eeb483d54a8b8d8a
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-05-26 09:54:47 -0300

    feat: create pipe zod validation and install rest client

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,7 +1,18 @@
-import { ConflictException } from "@nestjs/common";
+import { ConflictException, UsePipes } from "@nestjs/common";
 import { Body, Controller, HttpCode, Post } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+
 import { hash } from "bcryptjs"
+import { z } from "zod" 
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+
+const createAccountBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string(),
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
 
 @Controller('/accounts')
 export class CreateAccountController {
@@ -9,7 +20,8 @@ export class CreateAccountController {
 
     @Post()
     @HttpCode(201)
-    async handle(@Body() body: any){
+    @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+    async handle(@Body() body: CreateAccountBodySchema){
         const { name, email, password } = body;
 
         const userWithSameEmail = await this.prisma.user.findUnique({
