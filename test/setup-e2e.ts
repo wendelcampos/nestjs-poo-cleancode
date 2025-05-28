a40ac5c7336d30266b4fd4ef84cbc7e00cf6dea5
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-05-28 20:50:55 -0300

    chore: installed vitest

---
+++ test/setup-e2e.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from '@prisma/client';
+import { randomUUID } from 'node:crypto';
+import 'dotenv/config';
+import { execSync } from 'node:child_process';
+import { a } from 'vitest/dist/chunks/suite.d.FvehnV49';
+
+const prisma = new PrismaClient()
+
+function generateUniqueDatabaseURL(schemaId: string) {
+    if(!process.env.DATABASE_URL) {
+        throw new Error('Please provider a DATABASE_URL environment variable');
+    }
+
+    const url = new URL(process.env.DATABASE_URL)
+
+    url.searchParams.set('schema', schemaId);
+
+    return url.toString();
+}
+
+const schemaId = randomUUID();
+
+beforeAll(async () => {
+    const databaseURL = generateUniqueDatabaseURL(schemaId);
+
+    process.env.DATABASE_URL = databaseURL;
+
+    execSync('yarn prisma migrate deploy')
+})
+
+afterAll(async () => {
+
+    await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`);
+    await prisma.$disconnect();
+
+})
+
