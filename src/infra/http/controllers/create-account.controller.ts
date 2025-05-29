c0aecb0a2c0680ac8b0b81772e844aae839ef84d
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-05-29 13:30:01 -0300

    feat: created infra layer e refatored classes

--- src/infra/http/controllers/create-account.controller.ts
+++ src/infra/http/controllers/create-account.controller.ts
@@ -1,10 +1,10 @@
 import { ConflictException, UsePipes } from "@nestjs/common";
 import { Body, Controller, HttpCode, Post } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
 
 import { hash } from "bcryptjs"
 import { z } from "zod" 
-import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
 
 const createAccountBodySchema = z.object({
     name: z.string(),
--- src/infra/http/controllers/fetch-recent-question.controller.e2e-spec.ts
+++ src/infra/http/controllers/fetch-recent-question.controller.e2e-spec.ts
@@ -1,9 +1,8 @@
-import { AppModule } from "@/app.module";
-import { PrismaService } from "@/prisma/prisma.service";
+import { AppModule } from "@/infra/app.module";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
 import { INestApplication } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { Test } from "@nestjs/testing";
-import { title } from "process";
 import request from 'supertest';
 
 describe('Fetch recent questions (E2E)', () => {
