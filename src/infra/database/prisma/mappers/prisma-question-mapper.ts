c8d7b81d39479c6c5d03a8a656b21c86d61cbe4b
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-06-15 09:46:31 -0300

    feat: created all methods of PrismaQuestionsRepository

--- src/infra/database/prisma/mappers/prisma-question-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -1,7 +1,7 @@
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Question } from "@/domain/forum/enterprise/entities/question";
 import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
-import { Question as PrismaQuestion } from "@prisma/client";
+import { Question as PrismaQuestion, Prisma } from "@prisma/client";
 
 export class PrismaQuestionMapper {
     static toDomain(raw: PrismaQuestion): Question {
@@ -16,4 +16,17 @@ export class PrismaQuestionMapper {
         }, 
         new UniqueEntityID(raw.id))
     }
+
+    static toPrisma(question: Question): Prisma.QuestionUncheckedCreateInput {
+        return {
+            id: question.id.toString(),
+            title: question.title,
+            content: question.content,
+            authorId: question.authorId.toString(),
+            bestAnswerId: question.bestAnswerId?.toString(),
+            slug: question.slug.value,
+            createdAt: question.createAt,
+            updatedAt: question.updateAt,
+        }
+    }   
 }
