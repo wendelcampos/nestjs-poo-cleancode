402598ede68b2406cd784b9adefa0577937cde28
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-06-15 16:13:23 -0300

    feat: created stubs for encrypter and hasher

---
+++ test/criptography/fake-encrypter.ts
@@ -0,0 +1,8 @@
+import { Encrypter } from "@/domain/forum/application/cryptography/encrypter";
+
+export class FakeEncrypter implements Encrypter {
+    async encrypt(payload: Record<string, unknown>): Promise<string> {
+        return JSON.stringify(payload)
+    }
+ 
+}
