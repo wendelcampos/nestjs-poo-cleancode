ca07d9da74c1324d972bb02f042b14c39acaf461
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-06-16 15:56:55 -0300

    test: created get question by slug controller

---
+++ src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
@@ -0,0 +1,57 @@
+import { AppModule } from "@/infra/app.module";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { INestApplication } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Test } from "@nestjs/testing";
+import request from 'supertest';
+
+describe('Get question by slug (E2E)', () => {
+    let app: INestApplication;
+    let prisma: PrismaService
+    let jwt: JwtService
+    
+    beforeAll(async () => {
+        const moduleRef = await Test.createTestingModule({
+            imports: [AppModule],
+        })
+        .compile();
+
+        app = moduleRef.createNestApplication()
+
+        prisma = moduleRef.get(PrismaService);
+        jwt = moduleRef.get(JwtService);
+
+        await app.init();
+    });
+
+    test('[GET] /questions/:slug', async () => {
+        const user = await prisma.user.create({
+        data: {
+                name: 'John Doe',
+                email: 'jonhdoe@example.com',
+                password: '123456',
+            }
+        })
+
+        const accessToken = await jwt.sign({ sub: user.id });
+
+        await prisma.question.create({
+            data: {
+                title: 'Question 01',
+                slug: 'question-01',
+                content: 'Content of the first question',
+                authorId: user.id,
+            },
+        })
+                
+       const response = await request(app.getHttpServer())
+            .get('/questions/question-01')
+            .set('Authorization', `Bearer ${accessToken}`)
+            .send()
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({
+            questions: expect.objectContaining({ title: 'Question 01' }),
+        })
+    })
+})
