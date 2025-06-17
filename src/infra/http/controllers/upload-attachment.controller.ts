6164ac030e8c2c57f6063f21456a083b6308f975
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-06-17 16:06:19 -0300

    feat: created upload attachment controller

---
+++ src/infra/http/controllers/upload-attachment.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Post, UseInterceptors, UploadedFile, ParseFilePipe, MaxFileSizeValidator, FileTypeValidator } from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+
+@Controller('/attachments')
+export class UploadAttachmentController {
+    // constructor( ){}
+
+    @Post()
+    @UseInterceptors(FileInterceptor('file'))
+    async handle(@UploadedFile(
+        new ParseFilePipe({
+            validators: [
+            new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 2 }), // 2MB
+            new FileTypeValidator({ fileType: '.(png|jpg|jpeg|pdf)' }),
+            ],
+        }),
+    ) file: Express.Multer.File) {
+        console.log(file);
+    }
+}
